598d5b744b5ce8dd5931d8c9dd8dda2d66f71293
pacomunuera <46718931+pacomunuera@users.noreply.github.com>
2019-12-15 10:34:12 +0100

    20191215

--- csenergy/__init__.py
+++ csenergy/__init__.py
@@ -24,6 +24,8 @@ class HCE:
         self.sigma = 1
         self.tin = 1
         self.tout = 1
+        self.SCA = 1
+        self.SCAposition = 1
         
         
     def __PRBarbero4grade__(self):
@@ -32,42 +34,116 @@ class HCE:
     def __PRBarbero1grade__(self):
         return 0
     
+    def __PRBarbero0grade__(self):
+        return 0
+   
+    def calcTempOut(self):
+        return 0
+    
+    
 h1 = HCE()
 
 print(h1.__PRBarbero1grade__())
     
 class SCA(object):
-    def __init__(self, HCEperSCA=24):
-        self._HCEperSCA = 24
+    
+    self.HCEperSCA = 24
+    self.HCEArray = ()
+    self.Loop = 1
+    self.positionInLoop = 1
+    self.operationStatus = 0
+    self.SCAtemperature = 0
+    
+    def __init__(self, pIL, HCEperSCA=24):
+        self.positionInLoop = pIL
+    
+    def setOperationStatus():
         
-        
+        if self.SCAtemperature > self.SCATempMax():
+            self.operationStatus = 'defocused'
+        else:
+            self.operationStatus = 'tracking'
+            
+            
 class Loop(object):
     def __init__(self, SCAperLOOP = 4):
         self._SCAperLOOP = 4
         
+    def tempOut(self, weatherstatus, plantstatus):
+        '''
+        Calculation of the output temperature Tout of the HTF
+        according to the input temperature, mass flow, wheather
+        conditions, plant performance
+
+        Returns
+        -------
+        Output Temperature, Tout
+
+        '''
+        pass
+    
+    def massFlow():
+        '''
+        Calculation of the current mass flow of HTF in the loop
+        by dividing the total mass flow by the number of loops
+        in the plant 
+        
+
+        Returns
+        -------
+        current HTF massflow, massFlowLoop
+
+        '''
+
+        pass
+    
+    def requiredMassFlow():
+        '''
+        Calculation of the massFlow necessary to obtain the output temperature
+        required by the operator
+
+        Returns
+        -------
+        required HTF mass flow, reqMassFlowLoop
+
+        '''
+        pass
+    
 class Plant(object):
     def __init__(self, LOOPperPLANT=120):
         self._LOOPperPLANT = 120
         
 class Site(object):
     def __init__(self, lat=39, long=-3):
+        
         self._lat = 39
         self._long = 3
         
 class HTF(object):
+    
     def __init__(self, *argv):
         self._name = "Therminol VP-1"
 
-class Weather(object):
+class Weather(object):   
     def __init__(self, file):
-        self.file = 1
-        self.data = pd.read_csv("filename.csv")
+        pass
         
-''' Definimos la clase simulacion para representar las diferentes
-    pruebas que lancemos, variando el archivo TMY, la configuracion del
-    site, la planta, el modo de operacion o el modelo empleado. '''
+    def loadWheatherData(path):
+        self.wheatheData = read_tmy2(path)
+
+# format = '%Y-%m-%d %H:%M:%S'
+# df['Datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format=format)
+# df = df.set_index(pd.DatetimeIndex(df['Datetime']))
+        
+    
 
 class Simulation(object):
+    ''' 
+    Definimos la clase simulacion para representar las diferentes
+    pruebas que lancemos, variando el archivo TMY, la configuracion del
+    site, la planta, el modo de operacion o el modelo empleado. 
+    
+    '''
     def __init__(self, weather, site, plant, operation, model):
         self.t_init = 1
         self.t_end = 1
