6ee048d85831e79805d84c9b1734f4025c1c468c
pacomunuera <46718931+pacomunuera@users.noreply.github.com>
2019-12-19 17:23:13 +0100

    Update __init__.py

--- csenergy/__init__.py
+++ csenergy/__init__.py
@@ -12,6 +12,11 @@
 #from pvlib import spa
 import pandas as pd
 
+
+
+PI = 3.141592653589793
+
+
 #TODO: 1 Preparación dataframe inicial. A partir de CSV como punto de inicio.
 #TODO: 2 Preparación de datos del HCE
 
@@ -27,7 +32,7 @@ HCE_status = {"Hydrogen ": 0, "Vacuum ": 0, "Broken ": False}
 HCE_operation = {"Tin ": 293, "Clean ": 1}
 
 class HCE:
-    
+    long = 1
     def __init__(self, HCE_type: list, HCE_status: list, HCE_operation: list) -> None:
         self.long = 1
         self.Dro = 1
@@ -64,6 +69,9 @@ class HCE:
 
         '''
         return 0
+    def __len__(self):
+        print("coñe")
+        
     
     def qabs() -> float:
         '''
@@ -76,11 +84,8 @@ class HCE:
             Thermal power absorbed by the HCE
         '''
         return 0       
-    
-    
-h1 = HCE()
+ 
 
-print(h1.__PRBarbero1grade__())
 
 SCA_configuration ={"HCE Number ": 12, "Position in Loop": 1,
                     "Temperature probes number": 1,
@@ -88,18 +93,25 @@ SCA_configuration ={"HCE Number ": 12, "Position in Loop": 1,
 
    
 class SCA(object):
-    
-    HCEperSCA = 24
-    HCEArray = ()
-    Loop = 1
-    positionInLoop = 1
-    operationStatus = 0
-    SCAtemperature = 0
+    #Uso de __slots__ permite ahorrar memoria RAM
+    __slots__= ['SCA_configuration']
+
     
     def __init__(self, SCA_configuration):
+        
+        self.SCA_type = {    
+                'HCEperSCA': 24,
+                'Loop': 1,
+                'positionInLoop': 1,
+                'operationStatus': 0,
+                'SCAtemperature': 0
+                }
+        
         for i in range (1, self.HCEperSCA):
             self.HCElist.append(HCE(HCE_type, HCE_status, HCE_operation))       
         
+    def __gettitem__(self, item):
+        return self.SCA_type[item]
     
     def setOperationStatus():
         
@@ -159,7 +171,7 @@ class SolarField(object):
     
    
 class Plant(object):
-    def __init__(self, ):
+    def __init__(self ):
         self._LOOPperPLANT = 120
         
 class Site(object):
@@ -174,11 +186,11 @@ class HTF(object):
         self._name = "Therminol VP-1"
 
 class Weather(object):   
-    def __init__(self, file):
+    def __init__(self):
         pass
         
     def loadWheatherData(path):
-        self.wheatheData = read_tmy2(path)
+        pass
 
 # format = '%Y-%m-%d %H:%M:%S'
 # df['Datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format=format)
@@ -200,15 +212,56 @@ class Simulation(object):
     def result():
         pass
         return df
-        
+"""
+El usuario selecciona:
     
-weather_file = Weather()
-fluid = HTF()
-site = Site()
-plant = Plant()
+    - Fichero Weather, se crea dataframe con registros a intervalos temporales
+    - Configuración de planta (N_campos, N_lazos, N_SCA_lazo, N_HCE_SCA)
+    - Modelo de planta:
+        - Modelo 1: Parámetros sin dispersión
+        - Modelo 2: Parámetros con dispersión
+    - Tipo de simulacion:
+        - Simulacion 1: Comparación con datos de generacion
+        - Simulacion 2: Simulación de planta
+    
+    - Se genera la planta:
+        - Modelo 1: Por ser todos iguales no sería necesario crear N_lazosxN_SCA_lazoxN_HCE_SCA
+        sino que se va a recurrir a bucles para generar las salidas de temperatura y guardarlos
+        en el dataframe
+        
+        - Modelo 2: Se crean los N_lazosxN_SCA_lazoxN_HCE_SCA HCE con sus atributos cargados según 
+        las funciones de dispersión que toque. 
+        
+    - Simulación:
+        - Simulación 1: para cada registro temporal se toma el caudal y se calcula la temperatura de
+        salida. Hay que manejar las temperaturas por encima del setpoint como pérdidas de energía por 
+        culpa del desenfoque
+        
+        - Simulación 2:  para cada registro temporal se calcula el caudal para conseguir la temperatura
+        de salida.
+        
+        datos que debe almacenarse en cada HCE
+        - Rendimiento, tin, tout, massflow
+        
+        datos que debe almacenarse en cada SCA
+        - Rendimiento (promedio de sus HCE), tin, tout, t_probe,  massflow, status (desenfoque, etc...)
+        
+        datos que debe almacenarse en cada LOOP
+        - Rendimiento (promedio de sus SCA), tin, tout, massflow
+        
+        
+            
 
-simulation = Simulation(self, weather_file, plant, operation, model)
-simultation_output = simulation.result()
+
+"""        
+    
+#weather_file = Weather()
+#fluid = HTF()
+#site = Site()
+#plant = Plant()
+#
+#simulation = Simulation(self, weather_file, plant, operation, model)
+#simultation_output = simulation.result()
 
 
         
