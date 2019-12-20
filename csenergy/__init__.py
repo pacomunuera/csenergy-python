aff1eb5364b0bfb62590075fb51f5e0d7ffe95d4
pacomunuera <46718931+pacomunuera@users.noreply.github.com>
2019-12-20 14:15:55 +0100

    Update __init__.py

--- csenergy/__init__.py
+++ csenergy/__init__.py
@@ -32,7 +32,10 @@ HCE_status = {"Hydrogen ": 0, "Vacuum ": 0, "Broken ": False}
 HCE_operation = {"Tin ": 293, "Clean ": 1}
 
 class HCE:
-    long = 1
+    
+    
+    long = 1        
+    
     def __init__(self, HCE_type: list, HCE_status: list, HCE_operation: list) -> None:
         self.long = 1
         self.Dro = 1
@@ -68,12 +71,9 @@ class HCE:
             DESCRIPTION.
 
         '''
-        return 0
-    def __len__(self):
-        print("coñe")
-        
+        return 0      
     
-    def qabs() -> float:
+    def q_abs() -> float:
         '''
         Ec. 3.20 Barbero
 
@@ -96,9 +96,9 @@ class SCA(object):
     #Uso de __slots__ permite ahorrar memoria RAM
     __slots__= ['SCA_configuration']
 
+
     
     def __init__(self, SCA_configuration):
-        
         self.SCA_type = {    
                 'HCEperSCA': 24,
                 'Loop': 1,
@@ -106,6 +106,8 @@ class SCA(object):
                 'operationStatus': 0,
                 'SCAtemperature': 0
                 }
+        self.SCA_tin = SCA.firstHCEinSCA(self).HCE.tin
+        
         
         for i in range (1, self.HCEperSCA):
             self.HCElist.append(HCE(HCE_type, HCE_status, HCE_operation))       
@@ -121,7 +123,7 @@ class SCA(object):
             soperationStatus = 'tracking'
             
             
-class Loop(object):
+class PTLoop(object):
     def __init__(self, SCAperLOOP = 4):
         self._SCAperLOOP = 4
         
@@ -155,6 +157,7 @@ class Loop(object):
     
     def requiredMassFlow():
         '''
+        
         Calculation of the massFlow necessary to obtain the output temperature
         required by the operator
 
@@ -165,14 +168,63 @@ class Loop(object):
         '''
         pass
 
-class SolarField(object):
+class PTSolarField(object):
+    '''
+    Parabolic Trough Solar Field
+    
+    '''
+    
     def __init__(self, LoopsPerSolarField):
+        self.configuration = {'N_loops_per_solar_field': 30,
+                              'N_SCA_per_loop': 4,
+                              'N_HCE_per_SCA': 24
+                              }
+        self.massflow =1
+        self.tin= 1
+        self.tout = self.tin
+    
+    def get_configuration():
+        return self.configuration
+    
+    
+    def get_massflow(self):
+        '''
+        Calculates total massflow throughout the solar field as 
+        a sum of each massflow in a loop belonging the solar field
+        
+        '''
+        
+    def get_tout(self):
+        '''
+        Calculates HTF output temperature throughout the solar field as a 
+        weighted average based on the enthalpy of the mass flow in each 
+        loop belonging the solar field
+        
+        '''       
+        
+        
+        
+class PTPlant(object):
+    '''
+    Parabolic Trough Concentrated Solar Power Plant
+    
+    ''''
+    
+    def __init__(self ):
         pass
     
-   
-class Plant(object):
-    def __init__(self ):
-        self._LOOPperPLANT = 120
+    def set_configuration():
+        PTPlant_solarfields = ()        
+                              
+        
+        
+    def get_configuration():
+        for _ in self.PTPlant_solarfields:
+            configuration.add(_.configuration)
+        return configuration
+    
+    
+    
         
 class Site(object):
     def __init__(self, lat=39, long=-3):
@@ -192,6 +244,27 @@ class Weather(object):
     def loadWheatherData(path):
         pass
 
+class ScatterMask(object):
+    
+    _PARAMETERS = {'sigma': 1, 'clean_mirror': 1}
+    
+    def __init__(self, plant):
+        parameterMatrix = plant.configuration()
+        pass
+    
+    def define_scattered_parameters_model():
+        pass
+        return scatterd_parameters_model
+    
+    
+    def fill_mask(self, scattered_parameters_model):
+        
+        pass
+    
+    
+    pass
+
+
 # format = '%Y-%m-%d %H:%M:%S'
 # df['Datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'], format=format)
 # df = df.set_index(pd.DatetimeIndex(df['Datetime']))
@@ -205,13 +278,18 @@ class Simulation(object):
     site, la planta, el modo de operacion o el modelo empleado. 
     
     '''
-    def __init__(self, weather, site, plant, operation, model):
+    
+    def __init__(self, weather, site, plant, operation, model,df):
         self.t_init = 1
         self.t_end = 1
+        
+    def run():
+        pass
         
     def result():
         pass
         return df
+    
 """
 El usuario selecciona:
     
@@ -233,8 +311,8 @@ El usuario selecciona:
         las funciones de dispersión que toque. 
         
     - Simulación:
-        - Simulación 1: para cada registro temporal se toma el caudal y se calcula la temperatura de
-        salida. Hay que manejar las temperaturas por encima del setpoint como pérdidas de energía por 
+        - Simulación 1: para cada registro temporal se toma el caudal real y se calcula la temperatura de
+        salida con el modelo seleccionado. Hay que manejar las temperaturas por encima del setpoint como pérdidas de energía por 
         culpa del desenfoque
         
         - Simulación 2:  para cada registro temporal se calcula el caudal para conseguir la temperatura
@@ -248,16 +326,17 @@ El usuario selecciona:
         
         datos que debe almacenarse en cada LOOP
         - Rendimiento (promedio de sus SCA), tin, tout, massflow
-        
-        
-            
-
-
 """        
+
     
 #weather_file = Weather()
 #fluid = HTF()
 #site = Site()
+
+#sf = solarField()
+
+#mask = ScatterMask()
+
 #plant = Plant()
 #
 #simulation = Simulation(self, weather_file, plant, operation, model)
